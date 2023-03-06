38b9ac85c9d448ab5a4297af9aa76487f11dd9a8
Gustavo Dias <90855105+gustapine@users.noreply.github.com>
2023-03-06 20:03:38 -0300

    Update sizebay.js

--- src/js/sizebay.js
+++ src/js/sizebay.js
@@ -1,19 +1,20 @@
-function findEven (even){
+function findEven(returnEven=true) {
     const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     const even = []
     const odd = [];
+    
 
-    for(i=0; array.length > i; i++){
-        if(array[i] % 2==0){
-       even.push(array[i])
-        }else{
+    for (i = 0; array.length > i; i++) {
+        if (array[i] % 2 == 0) {
+            even.push(array[i])
+        } else {
             odd.push(array[i])
         }
-       }
-    
-    if(even){
-    alert("Os números pares são: "+even);
-    }else{
-    alert("Os números ímpares são: "+odd);
+    }
+
+    if (returnEven) {
+        alert("Os números pares são: " + even);
+    } else {
+        alert("Os números ímpares são: " + odd);
     }
 }
